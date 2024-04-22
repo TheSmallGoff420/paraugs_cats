319753fe067eb59f84e3e80c9ab62ca64056a2c6
TheSmallGoff420 <112856943+TheSmallGoff420@users.noreply.github.com>
2024-04-22 15:10:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(1)
+//console.log(2)
+let zina = document.querySelector('.zina');
+let zinas = document.querySelector('.cataZina');
+function sutitZinu()
+
+{ 
+    console.log ('function sutitZinu()darbojas');
+    zinas.innterHTML = zinas.innerHTML+ '<br>'+zina.value;
+    
+}
+function ieladetChataZinas()
+{
+    
+}
